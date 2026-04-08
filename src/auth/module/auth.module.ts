d3040ff750571c11fad26c4c670e3aafe67c00a0
iamogir <ia.mogirevsky@gmail.com>
2026-04-08 12:46:03 +0300

    auth module

--- src/auth/module/auth.module.ts
+++ src/auth/module/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
 import { AuthController } from '../controller/auth.controller';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: 'SECRET_KEY',
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
 })
 export class AuthModule {}
