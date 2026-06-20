db467c04b373502c06ff51786173404e2902643a
iamogir <ia.mogirevsky@gmail.com>
2026-06-20 16:37:53 +0300

    added service upload

--- src/storage/storage.service.ts
+++ src/storage/storage.service.ts
@@ -1,5 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { v2 as cloudinary } from 'cloudinary';
 
 @Injectable()
-
-export class StorageService {}
+export class StorageService {
+  async uploadImage(file: Express.Multer.File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        {
+          folder: 'pet-dom',
+          resource_type: 'image',
+        },
+        (error, result) => {
+          if (error) return reject(error as Error);
+          if (!result) {
+            return reject(new Error('Cloudinary upload failed'));
+          }
+          resolve(result.secure_url);
+        },
+      );
+      uploadStream.end(file.buffer);
+    });
+  }
+}
